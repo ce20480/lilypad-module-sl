e8b499ab6c6c1bd8e9b5e9e90aab931d847a3f14
ce20480 <aaronvinod25@gmail.com>
2025-04-12 18:31:05 +0100

    Updated inference script to be correct for lilypad not for local testing. If you want to go back to a previous commit do that to find the old files for testing

--- src/run_inference.py
+++ src/run_inference.py
@@ -1,61 +1,48 @@
+# standard library imports
 import os
 import sys
 import traceback
 import json
 
+# third party imports
 import numpy as np
 import torch
-# import argparse
 
+LETTER_MAPPING = {
+    0: "A",
+    1: "B",
+    2: "C",
+    3: "D",
+    4: "E",
+    5: "F",
+    6: "G",
+    7: "H",
+    8: "I",
+    9: "J",
+    10: "K",
+    11: "L",
+    12: "M",
+    13: "N",
+    14: "O",
+    15: "P",
+    16: "Q",
+    17: "R",
+    18: "S",
+    19: "T",
+    20: "U",
+    21: "V",
+    22: "W",
+    23: "X",
+    24: "Y",
+    25: "Z",
+}
 
 def run_job(landmarks, model):
     """
     Run the job
     """
     """Combine input file with working directory to get the full path"""
-    print(type(landmarks[0]))
-    print(landmarks)
-
     try:
-        # landmarks_array = np.array(landmarks)
-        # # Check if we have the right shape
-        # if landmarks_array.size == 1:
-        #     return {
-        #         "success": False,
-        #         "message": "Invalid landmarks format: Empty or single value"
-        #     }
-    
-        # # Ensure we have the correct shape (21 landmarks with 3 coordinates each)
-        # if len(landmarks) % 3 == 0:
-        #     # If flat array, reshape to (n, 3)
-        #     num_landmarks = len(landmarks_array) // 3
-        #     landmarks_array = landmarks_array.reshape(num_landmarks, 3)
-        # elif len(landmarks_array.shape) == 1:
-        #     # If it's still a 1D array but not divisible by 3, we have a problem
-        #     return {
-        #         "success": False,
-        #         "message": f"Invalid landmarks format: Cannot reshape array of size {landmarks_array.size} into landmarks"
-        #     }
-
-        # min_x = np.min(landmarks_array[:, 0])
-        # max_x = np.max(landmarks_array[:, 0])
-        # min_y = np.min(landmarks_array[:, 1])
-        # max_y = np.max(landmarks_array[:, 1])
-        
-        # # Calculate scale factors
-        # x_range = max_x - min_x
-        # y_range = max_y - min_y
-        
-        # if x_range == 0 or y_range == 0:
-        #     return landmarks_array
-        
-        # # Normalize coordinates to [0, 1] range
-        # normalized_landmarks = landmarks_array.copy()
-        # normalized_landmarks[:, 0] = (normalized_landmarks[:, 0] - min_x) / x_range
-        # normalized_landmarks[:, 1] = (normalized_landmarks[:, 1] - min_y) / y_range
-
-        # features = normalized_landmarks.flatten()
-
         features = np.array(landmarks).astype(np.float32)
         input_tensor = torch.tensor(features).unsqueeze(0)
 
@@ -66,37 +53,9 @@ def run_job(landmarks, model):
             pred_class = torch.argmax(probabilities, dim=1)
             confidence = torch.max(probabilities, dim=1).values
 
-        letter_mapping = {
-            0: "A",
-            1: "B",
-            2: "C",
-            3: "D",
-            4: "E",
-            5: "F",
-            6: "G",
-            7: "H",
-            8: "I",
-            9: "J",
-            10: "K",
-            11: "L",
-            12: "M",
-            13: "N",
-            14: "O",
-            15: "P",
-            16: "Q",
-            17: "R",
-            18: "S",
-            19: "T",
-            20: "U",
-            21: "V",
-            22: "W",
-            23: "X",
-            24: "Y",
-            25: "Z",
-        }
 
         # Get letter from prediction
-        letter = letter_mapping[pred_class.item()]
+        letter = LETTER_MAPPING[pred_class.item()]
 
         # Calculate a confidence score for evaluation (0.0-1.0)
         confidence = float(confidence.item())
@@ -123,16 +82,16 @@ def run_job(landmarks, model):
     return output
 
 
-def main(input=False):
+def main():
     print("Starting inference...")
 
-    if not input:
-        input = os.environ.get("INPUT")
+    # Get input from environment variable
+    input = os.environ.get("INPUT")
+    print("Input:", input, file=sys.stderr, flush=True)
     parsed_landmarks = json.loads(input)
 
     # `/app` directory aligns with the `WORKDIR` specified in the `Dockerfile`
-    # model_path = "/app/models/asl_model_20250310_184213.pt"
-    model_path = "models/asl_model_20250310_184213.pt"
+    model_path = "/app/models/asl_model_20250310_184213.pt"
     output = {"input": input, "status": "error"}
 
     try:
@@ -164,6 +123,8 @@ def main(input=False):
 
     except Exception as error:
         print("❌ Error during processing:", file=sys.stderr, flush=True)
+        print("Error for input:", output["input"], file=sys.stderr, flush=True)
+
         traceback.print_exc(file=sys.stderr)
         output["error"] = str(error)
 
@@ -182,9 +143,4 @@ def main(input=False):
 
 
 if __name__ == "__main__":
-    # for testing to see if the model is working in my local dev
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--input", "-i", type=str, required=True)
-    # args = parser.parse_args()
-    # main(args.input)
     main()
